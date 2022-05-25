5de04cb882f6843c9824c4f7d42b557f663f7b68
Abi Emery <abigail.emery@diamond.ac.uk>
2022-05-25 16:38:26 +0100

    Unify argument names between passfilter .h and .c

--- passfilter.h
+++ passfilter.h
@@ -1,9 +1,10 @@
 /* The headerfile for the actual "passfilter" */
 
 #include<string.h>
+#include<stdio.h>
 
-int compress(void* destination_buffer, unsigned long destination_length,
-    void* source_buffer,unsigned long nbytes);
+int compress(void* destination_buf, unsigned long destination_length,
+    void* source_buf,unsigned long nbytes);
 
 int decompress(void* destination_buf, unsigned long destination_buf_length,
     void* source_buf,unsigned long nbytes);
